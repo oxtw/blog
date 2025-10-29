451b3f13d0e5f8fe7528b0e16d9375e8fcdd5372
miguel <weigertmiguel@hotmail.com>
2025-10-29 13:29:50 -0300

    style: finaliza CSS do FeaturedPost com Tailwind

--- src/components/FeaturedPost/index.tsx
+++ src/components/FeaturedPost/index.tsx
@@ -14,19 +14,44 @@ export default function FeaturedPost() {
         "group"
       )}
     >
-      <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+      <Link
+        className={clsx("w-full", "h-full", "overflow-hidden", "rounded-xl")}
+        href="#"
+      >
         <Image
-          className="group-hover:scale-105 transition"
+          className={clsx(
+            "w-full",
+            "h-full",
+            "object-cover",
+            "object-center",
+            "group-hover:scale-105",
+            "transition"
+          )}
           src="/images/bryen_0.png"
           width={1200}
           height={720}
           alt="Título do post"
+          priority //carregando a imagem com prioridade
         />
       </Link>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui numquam
-        pariatur dolores explicabo. Fuga minus temporibus aut obcaecati tempora
-        pariatur ea quas, enim eius saepe quod repellendus ex veniam dolorem.
+      <div className="flex flex-col gap-4 sm:justify-center">
+        <time
+          className="text-slate-600 block text-sm/tight"
+          dateTime="2025-04-20"
+        >
+          20/04/2025 10:00
+        </time>
+
+        <h1 className={clsx("text-2xl/tight", "font-extrabold", "sm:text-4xl")}>
+          <Link href="#">Lorem ipsum dolor sit amet, consectetur</Link>
+        </h1>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui numquam
+          pariatur dolores explicabo. Fuga minus temporibus aut obcaecati
+          tempora pariatur ea quas, enim eius saepe quod repellendus ex veniam
+          dolorem.
+        </p>
       </div>
     </section>
   );
