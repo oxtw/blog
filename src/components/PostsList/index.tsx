d10e658c48a3ab2ec0902f26608bbf4dd5e60bf6
miguel <weigertmiguel@hotmail.com>
2025-11-10 08:48:00 -0300

    feat: exibindo data formatada nos componentes

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,6 +1,7 @@
 import { postRepository } from "@/repositories/post";
 import PostCoverImage from "../PostCoverImage";
 import PostHeading from "../PostHeading";
+import { formatDatetime, formatDistanceToNow } from "@/utils/format-datetime";
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
@@ -8,7 +9,6 @@ export async function PostsList() {
   return (
     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => {
-
         const postLink = `/post/${post.slug}`;
 
         return (
@@ -29,8 +29,9 @@ export async function PostsList() {
               <time
                 className="text-slate-600 block text-sm/tight"
                 dateTime={post.createdAt}
+                title={formatDistanceToNow(post.createdAt)}
               >
-                {post.createdAt}
+                {formatDatetime(post.createdAt)}
               </time>
 
               <PostHeading url={postLink} as="h2">
--- src/utils/format-datetime.ts
+++ src/utils/format-datetime.ts
@@ -1,22 +1,22 @@
-import { format, formatDistanceToNow } from "date-fns";
+import {
+  format,
+  formatDistanceToNow as dateFnsFormatDistanceToNow,
+} from "date-fns";
 import { ptBR } from "date-fns/locale";
 
-
-export function formatDatetime(rawDate: string): string{
+export function formatDatetime(rawDate: string): string {
   const date = new Date(rawDate);
 
-  return format(date, "dd/MM/yyyy HH:mm", { locale: ptBR});
+  return format(date, "dd/MM/yyyy HH:mm", { locale: ptBR });
 }
 
-export function formatRelativeDate(rawDate: string): string{
+export function formatDistanceToNow(rawDate: string): string {
   const date = new Date(rawDate);
 
-  return formatDistanceToNow(date, {
+  return dateFnsFormatDistanceToNow(date, {
     locale: ptBR,
     addSuffix: false,
   });
 }
 const rawDate = "2024-06-15T12:34:56Z";
 console.log(rawDate);
-console.log(formatRelativeDate(rawDate));
-
