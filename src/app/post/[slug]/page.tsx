21ffb09ea74d660e6f20abd10e3dfdf5bd47ad33
miguel <weigertmiguel@hotmail.com>
2025-11-14 13:30:30 -0300

    refactor: melhorando função findPostBySlugCached para reuso

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,26 +1,17 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
-import { notFound } from "next/navigation";
+
 
 type PostSlugPageProps = {
-  params: Promise<{slug: string}>;
-}
+  params: Promise<{ slug: string }>;
+};
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
-
-  let post;
-
-  try {
-  post = await findPostBySlugCached(slug);
-} catch  {
-  post = undefined;
-}
-
-if(!post) notFound();
+  const post = await findPostBySlugCached(slug);
 
   return (
     <div>
       <p>{post.content}</p>
     </div>
-  )
+  );
 }
--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -1,14 +1,19 @@
 import { postRepository } from "@/repositories/post";
+import { notFound } from "next/navigation";
 import { cache } from "react";
 
 export const findAllPublicPostsCached = cache(
   async () => await postRepository.findAllPublic()
 );
 
-export const findPostBySlugCached = cache(
-  async (slug: string) => await postRepository.findBySlug(slug),
-);
+export const findPostBySlugCached = cache(async (slug: string) => {
+  const post = await postRepository.findBySlug(slug).catch(() => undefined);
+
+  if (!post) return notFound();
+
+  return post;
+});
 
 export const findPostByIdCached = cache(
-  async (id: string) => await postRepository.findById(id),
+  async (id: string) => await postRepository.findById(id)
 );
