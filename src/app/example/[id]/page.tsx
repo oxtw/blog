10a9038eb2a59cf6bb61c1851d5844eb65ef13de
miguel <weigertmiguel@hotmail.com>
2026-03-21 18:47:53 -0300

    feat: utilizando server action para revalidação

--- src/app/example/[id]/page.tsx
+++ src/app/example/[id]/page.tsx
@@ -1,23 +1,28 @@
+import { revalidateExampleAction } from "@/actions/revalidate-example";
 import { formatHour } from "@/utils/format-datetime";
 
-
 export const dynamic = "force-static";
-export const revalidate = 10;
+// export const revalidate = 10;
 
-// tornando a rota estatica
-// export const dynamicParams = true;
-
-// export async function generateStaticParams() {
-//   return [{ id: "1" }, { id: "2" }, { id: "3" }];
-// }
-
-export default async function ExampleDynamicPage({params,}: {params: Promise<{id: string}>;}) {
-  const { id } =  await params;
+export default async function ExampleDynamicPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const hour = formatHour(Date.now());
 
   return (
     <main className="min-h-[600px] text-xl font-bold">
-      <div> Hora: {hour}(ID: {id})</div>
+      <div>
+        {" "}
+        Hora: {hour}(ID: {id})
+      </div>
+
+      <form name="path" className="py-16" action={revalidateExampleAction}>
+        <input type="hidden" defaultValue={`/example/${id}`} />
+        <button className="bg-amber-500 text-white p-2 rounded hover:bg-amber-600 transition cursor-pointer" type="submit">REVALIDATE</button>
+      </form>
     </main>
   );
 }
