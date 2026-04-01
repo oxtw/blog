ce1735177e0b3a85a23bb13ecd8a9760191ca4dc
miguel <weigertmiguel@hotmail.com>
2026-04-01 19:18:41 -0300

    feat: usando revalidatePath para revalidar o cache manualmente

--- src/actions/revalidate-example.ts
+++ src/actions/revalidate-example.ts
@@ -1,7 +1,10 @@
 'use server';
 
+import { revalidatePath } from "next/cache";
+
 export async function revalidateExampleAction(formData: FormData) {
   const path = formData.get("path")|| "";
-
   console.log("Revalidating...", path);
+
+  revalidatePath(`${path}`);
 }
