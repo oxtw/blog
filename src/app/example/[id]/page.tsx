daf59815a4503e4b22f5e4c193a1ec2b328a673e
miguel <weigertmiguel@hotmail.com>
2026-04-01 20:33:05 -0300

    feat: utilizando data cache com fetch e revalidateTag

--- src/app/example/[id]/page.tsx
+++ src/app/example/[id]/page.tsx
@@ -1,8 +1,8 @@
 import { revalidateExampleAction } from "@/actions/revalidate-example";
 import { formatHour } from "@/utils/format-datetime";
 
-export const dynamic = "force-static";
-export const revalidate = 30;
+// export const dynamic = "force-static";
+// export const revalidate = 30;
 
 export default async function ExampleDynamicPage({
   params,
@@ -12,16 +12,30 @@ export default async function ExampleDynamicPage({
   const { id } = await params;
   const hour = formatHour(Date.now());
 
+  const response = await fetch("https://randomuser.me/api/?results=1",{
+    next: {
+      tags: [],
+      revalidate: 30,
+    },
+  });
+
+  const json = await response.json();
+  const name = json.results[0].name.first;
+
   return (
     <main className="min-h-[600px] text-xl font-bold">
       <div>
-        {" "}
-        Hora: {hour}(ID: {id})
+       Name: {name}  |  Hora: {hour}(ID: {id})
       </div>
 
       <form name="path" className="py-16" action={revalidateExampleAction}>
         <input type="hidden" defaultValue={`/example/${id}`} />
-        <button className="bg-amber-500 text-white p-2 rounded hover:bg-amber-600 transition cursor-pointer" type="submit">REVALIDATE</button>
+        <button
+          className="bg-amber-500 text-white p-2 rounded hover:bg-amber-600 transition cursor-pointer"
+          type="submit"
+        >
+          REVALIDATE
+        </button>
       </form>
     </main>
   );
