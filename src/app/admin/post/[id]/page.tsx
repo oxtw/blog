ca0b201c1346eb21a85efebc537038a45fb7e54b
miguel <weigertmiguel@hotmail.com>
2026-04-07 15:56:46 -0300

    feat: Criando as rotas da área administrativa do site (admin)

---
+++ src/app/admin/post/[id]/page.tsx
@@ -0,0 +1,12 @@
+export const dynamic = "force-dynamic";
+
+type AdminPostIdPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function AdminPostIdPage({
+  params,
+}: AdminPostIdPageProps) {
+  const { id } = await params;
+  return <div className="p-16 text-6xl">AdminPostIdPage {id}</div>;
+}
---
+++ src/app/admin/post/new/page.tsx
@@ -0,0 +1,5 @@
+export const dynamic = 'force-dynamic';
+
+export default async function AdminPostNewPage() {
+  return <div className="p-16 text-6xl">AdminPostNewPage</div>
+}
