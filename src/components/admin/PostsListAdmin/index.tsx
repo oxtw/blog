23635a00885342e869ee7ad499d80d36a45b177a
miguel <weigertmiguel@hotmail.com>
2026-04-15 14:14:51 -0300

    feat: criar layout de dialog com Tailwind CSS
    Implementado layout de caixa de diálogo (dialog) utilizando Tailwind CSS
    para exibição de ações e interações na interface administrativa.

--- src/components/admin/PostsListAdmin/index.tsx
+++ src/components/admin/PostsListAdmin/index.tsx
@@ -29,10 +29,69 @@ export default async function PostsListAdmin() {
               </span>
             )}
 
-           <DeletePostButton id={post.id} title={post.title} />
+            <DeletePostButton id={post.id} title={post.title} />
           </div>
         );
       })}
+
+      <div
+        className={clsx(
+          "fixed",
+          "z-50",
+          "inset-0",
+          "bg-black/50",
+          "backdrop-blur-xs",
+          "flex items-center justify-center",
+        )}
+      >
+        <div
+          className={clsx(
+            "bg-slate-100",
+            "p-6",
+            "rounded-lg",
+            "max-w-2xl",
+            "mx-6",
+            "flex flex-col gap-6",
+            'shadow-lg shadow-black/30',
+
+          )}
+        >
+          <h3 className="text-xl font-extrabold">Titulo do dialogo</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius iure
+            hic quae cum excepturi laboriosam sed deserunt dolor inventore eaque
+            optio, perspiciatis ducimus commodi consequuntur earum, doloribus
+            sapiente illo sunt!
+          </p>
+          <div className="flex items-center justify-around">
+            <button
+              className={clsx(
+                "bg-slate-300",
+                "hover:bg-slate-400",
+                "transition",
+                "text-slate-950",
+                "flex items-center justify-center",
+                "py-2 px-4 rounded-lg cursor-pointer",
+              )}
+              autoFocus
+            >
+              Cancelar
+            </button>
+            <button
+              className={clsx(
+                "bg-blue-500",
+                "hover:bg-slate-600",
+                "transition",
+                "text-blue-50",
+                "flex items-center justify-center",
+                "py-2 px-4 rounded-lg cursor-pointer",
+              )}
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
