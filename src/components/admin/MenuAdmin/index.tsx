a8326e87d47c099989324b3eb5c023ad752e9eb3
Miguel Weigert <weigertmiguel@hotmail.com>
2026-05-12 11:49:51 -0300

    feat: adicionar lógica de abertura e fechamento no MenuAdmin
    Implementada a lógica de controle do MenuAdmin utilizando useState,
    useEffect e usePathname para abrir, fechar e resetar o menu ao navegar entre rotas.

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,25 +1,67 @@
-import clsx from "clsx";
-import { FileTextIcon, HouseIcon } from "lucide-react";
-import Link from "next/link";
+"use client";
 
-const navClasses = clsx(
-  "bg-slate-900 text-slate-100 rounded-lg",
-  "flex flex-col  mb-8",
-  "sm:flex-row sm:flex-wrap",
-  // 'h-10',
-  // 'overflow-hidden',
-);
-const linkClasses = clsx(
-  "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4",
-  "flex items-center justify-start gap-2",
-  "transition hover:bg-slate-800 rounded-lg",
-  "h-10",
-  "shrink-0",
-);
+import clsx from "clsx";
+import {
+  CircleXIcon,
+  FileTextIcon,
+  HouseIcon,
+  MenuIcon,
+  PlusIcon,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function MenuAdmin() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  const navClasses = clsx(
+    "bg-slate-900 text-slate-100 rounded-lg",
+    "flex flex-col  mb-8",
+    "sm:flex-row sm:flex-wrap",
+    !isOpen && "h-10",
+    !isOpen && "overflow-hidden",
+    "sm:overflow-visible sm:h-auto",
+  );
+  const linkClasses = clsx(
+    "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4",
+    "flex items-center justify-start gap-2",
+    "transition hover:bg-slate-800 rounded-lg",
+    "h-10",
+    "shrink-0",
+  );
+
+  const OpenCloseBtnClasses = clsx(
+    linkClasses,
+    "text-blue-200 italic sm:hidden",
+  );
+
   return (
     <nav className={navClasses}>
+      <button
+        onClick={() => setIsOpen((s) => !s)}
+        className={OpenCloseBtnClasses}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
+
       <a className={linkClasses} href="/" target="_blank">
         <HouseIcon />
         Home
@@ -30,39 +72,9 @@ export function MenuAdmin() {
         Posts
       </Link>
 
-      <Link className={linkClasses} href="/admin/post">
-        <FileTextIcon />
-        Posts
-      </Link>
-
-      <Link className={linkClasses} href="/admin/post">
-        <FileTextIcon />
-        Posts
-      </Link>
-
-      <Link className={linkClasses} href="/admin/post">
-        <FileTextIcon />
-        Posts
-      </Link>
-
-      <Link className={linkClasses} href="/admin/post">
-        <FileTextIcon />
-        Posts
-      </Link>
-
-      <Link className={linkClasses} href="/admin/post">
-        <FileTextIcon />
-        Posts
-      </Link>
-
-      <Link className={linkClasses} href="/admin/post">
-        <FileTextIcon />
-        Posts
-      </Link>
-
-      <Link className={linkClasses} href="/admin/post">
-        <FileTextIcon />
-        Posts
+      <Link className={linkClasses} href="/admin/post/new">
+        <PlusIcon />
+        Criar Post
       </Link>
     </nav>
   );
