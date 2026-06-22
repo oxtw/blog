0ea4a2c6f905a5aa4b4e03ed4e280d27615f450d
Miguel Weigert <weigertmiguel@hotmail.com>
2026-06-22 14:02:37 -0300

    feat: criar componente InputCheckBox reutilizável para formulários
    Criado o componente InputCheckBox utilizando Tailwind CSS,
    clsx e tipagem com TypeScript para padronizar campos do
    tipo checkbox na aplicação.
    
    O componente foi desenvolvido com suporte a label
    opcional, utilizando useId do React para gerar IDs
    únicos automaticamente e associar corretamente o label
    ao input através do htmlFor.
    
    Também foram adicionados estilos de foco e suporte às
    propriedades nativas da tag input por meio de
    React.ComponentProps<input>, permitindo maior
    reutilização e flexibilidade nos formulários.

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/Button";
+import { InputCheckBox } from "@/components/InputCheckBox";
 import { InputText } from "@/components/InputText";
 
 export const dynamic = "force-dynamic";
@@ -8,31 +10,35 @@ export const metadata = {
 
 export default async function AdminPostNewPage() {
   return (
-    <>
+    <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
-        <div>
-          <InputText labelText="Nome" placeholder="Digite seu nome" />
-          <InputText labelText="Sobrenome" placeholder="Digite seu Sobrenome" />
-          <InputText
-            disabled
-            labelText="Sobrenome"
-            placeholder="Digite seu Sobrenome"
-            defaultValue="Teste"
-          />
-          <InputText
-            disabled
-            labelText="Sobrenome"
-            placeholder="Digite seu Sobrenome"
-            readOnly
-          />
-          <InputText
-            readOnly
-            labelText="Sobrenome"
-            defaultValue="Teste"
-            placeholder="Digite seu Sobrenome"
-          />
-        </div>
+        <InputText labelText="Nome" placeholder="Digite seu nome" />
+        <InputText labelText="Sobrenome" placeholder="Digite seu Sobrenome" />
+        <InputCheckBox labelText="Sobrenome" />
+
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu Sobrenome"
+          defaultValue="Teste"
+        />
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu Sobrenome"
+          readOnly
+        />
+        <InputText
+          readOnly
+          labelText="Sobrenome"
+          defaultValue="Teste"
+          placeholder="Digite seu Sobrenome"
+        />
       </div>
-    </>
+
+      <div className="mt-4">
+        <Button type="submit">Enviar</Button>
+      </div>
+    </form>
   );
 }
